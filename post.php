f628734188952680e3f84df360873de557c4e4a9
y-sakamoto-solty <104179343+y-sakamoto-solty@users.noreply.github.com>
2022-04-24 11:01:21 +0900

    Add files via upload

---
+++ post.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = $_POST["name"];
+$sex = $_POST["sex"];
+$adress = $_POST["adress"];
+$tel = $_POST["tel"];
+$email = $_POST["email"];
+$comment = $_POST["comment"];
+
+include "conf.html";
+
+?>
